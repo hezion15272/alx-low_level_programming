8c7eef228ef1a10af1c972e41275b4e77e9b8274
hezion15272 <hezekiahibok@gmail.com>
2021-06-18 12:33:43 +0000

    Task 1 the last digit include in 0x01-variables

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/** main = assigns a random number to int n everytime
+ * it analyise the value assign to n
+ * and get the last digit
+ * it executes, and print the output 
+ * betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	int lastDigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	scanf("%d", &n);
+	lastDigit = n % 10;
+	if (lastDigit > 5)
+		printf("Last digit of %i is %d and is greater than 5\n", n, lastDigit);
+	else if (lastDigit == 0)
+		printf("Last digit of %i is %d and is 0\n", n, lastDigit);
+	else if (lastDigit < 6 != 0)
+		printf("Last digit of %i is %d and is less than 6 and not 0\n", n, lastDigit);
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,3 @@
 file 0-positive_or_negative.c assigns a random number to the variable n each time the code is executed.
+
+file 1 the last digit assigns a random number to the variable n each time it is executed.
