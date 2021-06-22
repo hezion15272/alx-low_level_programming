51739976bf2e82714f144b9db0b82d401e4c8e76
hezion15272 <hezekiahibok@gmail.com>
2021-06-22 18:41:09 +0000

    Task 5 through 11 added to 0x02-functions

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -13,4 +13,18 @@ int _islower();
 
 int _isalpha();
 
+int print_sign();
+
+int _abs();
+
+int print_last_digit();
+
+void jack_bauer();
+
+void times_table();
+
+int add();
+
+void print_to_98();
+
 #endif
