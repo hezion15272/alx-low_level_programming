a6ebcef4e75f7e279b6737191564ab7fa16417cf
Hezekiah Ibok <77856161+hezion15272@users.noreply.github.com>
2021-06-22 15:12:54 +0100

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,10 +1,21 @@
-#include <stdio.h>
+#include "holberton.h"
+
 /**
- * main- contains a program to print Holberton
- * Return: Always returns 0 (Success)
+ * main -  prints Holberton, followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("Holberton\n");
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
 	return (0);
 }
