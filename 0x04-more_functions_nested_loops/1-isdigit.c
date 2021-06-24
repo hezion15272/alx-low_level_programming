4ccb21b9d342233a4698447a3128d8193ea882ae
hezion15272 <hezekiahibok@gmail.com>
2021-06-24 14:59:27 +0000

    Task 1 isdigit included in 0x04-more

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * _isdigit - is a function that checks for a digit
+ * @c: is the checker
+ * Returns: 1 if c is a digit, otherwise 0
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,3 @@
 file 0-isupper.c writes a functions that checks for uppercase characters
+
+file 1-isdigit.c writes a function that checks for a digit
