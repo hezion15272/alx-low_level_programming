523bc251d70c8d617b33166df0669bc689946115
hezion15272 <hezekiahibok@gmail.com>
2021-06-24 14:25:52 +0000

    Task 0 isupper included in 0x04-more

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * isupper - is the function that returns uppercase
+ * @c: is the character to checked
+ * Return: 1 if c is uppercase, otherwise 0
+ */
+int _isupper(int c)
+{
+	return(c >= 'A' && c <= 'Z');
+}
