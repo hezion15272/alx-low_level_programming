f3a371f0f64d25d41a9e4b367d618125744ae20f
hezion15272 <hezekiahibok@gmail.com>
2021-06-24 15:17:27 +0000

    Task 2 collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: is a variable
+ * @b: is also a variable
+ * Return: mul on success
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,5 @@
 file 0-isupper.c writes a functions that checks for uppercase characters
 
 file 1-isdigit.c writes a function that checks for a digit
+
+file 2-mul.c writes a function that multiplies two integers
