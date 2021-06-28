8eb0a800eef742541d2887c3f4ce1ac0fbe6ac5d
hezion15272 <hezekiahibok@gmail.com>
2021-06-28 19:17:30 +0000

    Task 1 don't swap horses in crossing a stream included in 0x05-pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * swap_int - swaps the value of two variables
+ * @a: is the first variable
+ * @b: is the second variable
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42, *b = 98;
+}
