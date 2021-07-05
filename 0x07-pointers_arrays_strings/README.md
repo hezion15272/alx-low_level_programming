880250eac33fee5a7a29dd427865a064984cb577
Hezekiah Ibok <77856161+hezion15272@users.noreply.github.com>
2021-07-05 19:22:22 +0100

    Task 1 memcpy included in 0x07-pointers

--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,3 +1,3 @@
 file 0-memset.c writes a function that fills memory with a constant byte.
 
-
+file 1-memcpy.c creates a function that copies memory area.
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,3 +1,10 @@
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
+
+int _putchar(char c);
+
 char *_memset(char *s, char b, unsigned int n);
 
+char *_memcpy(char *dest, char *srcm unsigned int n);
 
+#endif
