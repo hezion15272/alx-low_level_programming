d6e2293604b01ff1c5048a0cd5c4c002e38e3e91
Hezekiah Ibok <77856161+hezion15272@users.noreply.github.com>
2021-07-12 20:04:15 +0100

    Task 2 add to 0x0A-argc_argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (success)
+ */
+
+int main (int argc, char *argv[]__attribute((unused)))
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
