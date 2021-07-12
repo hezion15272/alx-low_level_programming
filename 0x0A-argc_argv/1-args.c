16736d4c2e7d578fe034f1d9dc2c3f05718d16a2
Hezekiah Ibok <77856161+hezion15272@users.noreply.github.com>
2021-07-12 19:54:18 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - print the number of arguments passed to the program
- * @argc: counts the number of arguments passed
- * @argv: displays an array of arguments recieved
- * Return: Always 0 (Success)
+ * main - prints the name number of arguments passed to it
+ * @argc: counts the arguments passed
+ * @argv: an array of arguments passed to program
+ * Return: an interger 0 for sucess.
  */
-int main(int argc, char *argv[]__attribute__((unused)))
+int main(int argc, char *argv[]__attribute((unused)))
 {
-	printf("%d\n", argc -1);
-	return (0);
+
+printf("%d\n", argc - 1);
+return (0);
 }
