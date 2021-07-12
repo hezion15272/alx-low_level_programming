587e811b8990acd6433d4b4d25a88f6d2e19d389
Hezekiah Ibok <77856161+hezion15272@users.noreply.github.com>
2021-07-12 20:06:45 +0100

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,10 +4,10 @@
  * main - prints all arguments it receives
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: Always 0 (success)
+ *
+ * Return: Always 0 (Success)
  */
-
-int main (int argc, char *argv[]__attribute((unused)))
+int main(int argc, char *argv[])
 {
 	int i;
 
