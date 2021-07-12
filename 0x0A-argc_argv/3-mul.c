bee3b17f76e5ccd4c072c983a87052045bc4eb03
Hezekiah Ibok <77856161+hezion15272@users.noreply.github.com>
2021-07-12 20:15:32 +0100

    Task 3 added to 0x0A-argc_argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
+int main (int argc, char *argv[])
+{
+	int i = argc - 1;
+	if (i != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[i]) * atoi(argv[i - 1]));
+		return (0);
+	}
+}
