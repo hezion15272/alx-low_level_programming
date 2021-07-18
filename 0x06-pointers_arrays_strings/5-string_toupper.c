5f40910899ed12c1fa64cd5309272d83ce5cab15
hezion15272 <hezekiahibok@gmail.com>
2021-07-18 00:34:26 +0000

    Task 5 completed

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * string_toupper - changes all lowercase letters to a string to uppercase
+ * @s: holds the content of the string
+ *
+ * Return: output the results
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s [i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+
+	return (s);
+}
