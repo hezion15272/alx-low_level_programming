8b7a14d60d54a5003df1b690bce2b89740aaa78d
hezion15272 <hezekiahibok@gmail.com>
2021-07-19 15:17:22 +0000

    Task 2 included in 0x0E-structures

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_dog - is a function that prints the structure of dog
+ * @d: is the structure tag
+ */
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	if (d == NULL)
+		return;
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
