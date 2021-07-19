4c07b6df5f068f9458af81d2e1a42760accaac47
hezion15272 <hezekiahibok@gmail.com>
2021-07-19 14:06:08 +0000

    Task 0 Poppy included in 0x0E-structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H_
 #define DOG_H_
-
+/**
+ * struct dog - this the structure tag that contains the elements
+ * @name: is the variable that holds the name of the object
+ * @age: is the variable that holds the value of the age of the object
+ * @owner: is the variable that holds the name of the owner
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
